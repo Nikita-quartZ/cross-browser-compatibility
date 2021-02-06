42ac05f155765617f00e70e2151307c0d163b3b9
Nikita-quartZ <Flashfly16@gmail.com>
2021-02-06 23:12:00 +0400

    mini

---
+++ script/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+      
+        pagination: {
+          el: '.swiper-pagination',
+        }
+      });
+})
